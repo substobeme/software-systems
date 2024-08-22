fa1e40627fcea9b972bac1754e2fdf7d85a6823b
substobeme <suboff145@gmail.com>
2024-08-22 09:23:55 +0530

    committing

---
+++ 8.c
@@ -0,0 +1,41 @@
+/*Name: Subha Chakraborty
+  RegNo: MT2024156
+  ProblemStatement:Write a program to open a file in read only mode, read line by line and display each line as it is read.
+Close the file when end of file is reached.*/ 
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main(int c, char* argv[]){
+if(c!=1) { perror("the file doesn't exit");}
+
+int fd = open(argv[1],O_RDONLY);
+if(fd==-1) perror("No such file");
+char buffer[5000];
+int  buff = read(fd,buffer,sizeof(buffer));
+int move =0;
+for(int i=0;i<buff;i++){
+
+if(buffer[i]=='\n'){
+write(1,buffer+move,i+1-move);
+printf("The program has read one line\n");
+move = i+1;
+i=i+1;
+
+}
+
+}
+}
+/*terminal input: ./a.out 8.1
+ output: the file doesn't exit: Success
+this is line1.
+The program has read one line
+this is line2.
+The program has read one line
+this is line 3
+The program has read one line
+this is line 4.
+The program has read one line
+this is line 5.
+The program has read one line
+*/
