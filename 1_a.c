e2113ee0886642b96cdc62beaac4118792f18634
substobeme <suboff145@gmail.com>
2024-08-24 09:26:35 +0530

    made changes

--- 1_a.c
+++ 1_a.c
@@ -7,8 +7,13 @@ a. soft link (symlink system call) */
 #include<stdio.h>
 #include<unistd.h>
 
-int main(void){
-int catch= symlink("file3","softie3");
-if(catch== -1){printf("failed to create");
+int main(int c, char *argv[]){
+
+int check= symlink(argv[1],argv[2]);
+if(check== -1){printf("failed to create softlink");
 }
 }
+/*Terminal:./a.out file3 sflink1
+ 
+ Output:  
+lrwxrwxrwx 1 subha-chakraborty subha-chakraborty     1 Aug 24 09:19  sflink1 -> file3 */
--- 1_b.c
+++ 1_b.c
@@ -7,7 +7,10 @@ b. hard link (link system call) */
 #include<unistd.h>
 #include<stdio.h>
 
-int main(){
-int catch = link("hrd","hrd2");
-if(catch==-1)printf("Failed to link");
+int main(int c, char* argv[]){
+int check = link(argv[1],argv[2]);
+if(check==-1)printf("Failed to link");
 }
+/* Terminal: ./a.out hrd1 hrd2
+   Output:6179067 -rw-rw-r-- 2 subha-chakraborty subha-chakraborty     0 Aug 24 09:22  hrd1
+6179067 -rw-rw-r-- 2 subha-chakraborty subha-chakraborty     0 Aug 24 09:22  hrd2 */
