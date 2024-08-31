2c4d649d861cd8eb4c9bed53da3b548ae02ffc06
substobeme <suboff145@gmail.com>
2024-08-31 15:23:13 +0530

    Added Header

--- q18/18a.c
+++ q18/18a.c
@@ -1,10 +1,14 @@
-/* Name: Subha Chakraborty
-   RegNo: MT2024156
-   ProblemStatement:18. Write a program to perform Record locking.
-a. Implement write lock
-b. Implement read lock
-Create three records in a file. Whenever you access a particular record, first lock it then modify/access
-to avoid race condition.*/ 
+/*========================================================================================
+  ========================================================================================
+   Name: 18a.c
+   Author: Subha Chakraborty
+   Description: Write a program to perform Record locking.
+                a. Implement write lock
+                Create three records in a file. Whenever you access a particular record, first lock it then modify/access
+                to avoid race condition.
+   Date: 24th August,2024
+  =======================================================================================
+  =======================================================================================*/ 
 
 
 
