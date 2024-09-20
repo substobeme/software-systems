006e7f135a0c597621a6a62e15c54d484d5d3a4d
substobeme <suboff145@gmail.com>
2024-09-20 16:12:01 +0530

    Commit

---
+++ hands on list2/15.c
@@ -0,0 +1,41 @@
+/*========================================================================================
+  ========================================================================================
+   Name: 15.c
+   Author: Subha Chakraborty
+   Description:15. Write a simple program to send some data from parent to the child process.
+   the signal or not.
+   Date: 20th September,2024
+  =======================================================================================
+  =======================================================================================*/ 
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+int fd[2];
+char buf[4000];
+if(pipe(fd)==-1){
+perror("piped not created\n");
+exit(1);
+}
+if(!fork()){
+close(fd[1]);
+read(fd[0],buf,sizeof(buf));
+printf("The content of the pipe were : %s\n",buf);
+close(fd[0]);
+}
+else{
+close(fd[0]);
+char s[4000];
+printf("Enter your message:");
+scanf("%s",s);
+write(fd[1],s,strlen(s));
+close(fd[1]);
+}
+
+}
+/*Output:
+Enter your message:Sup_Child
+The content of the pipe were : Sup_Child
+
+*/
