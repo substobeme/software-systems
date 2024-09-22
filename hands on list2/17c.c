86c74f1fae2a4c67fd0cb1a545a1887e4bc76a0f
substobeme <suboff145@gmail.com>
2024-09-22 11:21:56 +0530

    Mods

--- hands on list2/17c.c
+++ hands on list2/17c.c
@@ -1,6 +1,6 @@
 /*========================================================================================
   ========================================================================================
-   Name: 17a.c
+   Name: 17c.c
    Author: Subha Chakraborty
    Description:17. Write a program to execute ls -l | wc.
    a. use dup
@@ -9,31 +9,30 @@
    Date: 21st September,2024
   =======================================================================================
   =======================================================================================*/
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
+
+#include <stdio.h>    
+#include <unistd.h>   
+#include <fcntl.h>    
+#include <sys/wait.h> 
 
 int main() {
-int fd[2];
-pipe(fd);
-if(!fork()) {
-close(fd[0]);
-close(STDOUT_FILENO);
-dup(fd[1]);
-close(fd[1]);
-execlp("ls", "ls", "-l", NULL);}
-if(!fork()) {
-close(fd[1]);
-close(STDIN_FILENO);
-dup(fd[0]);
-close(fd[0]);
-execlp("wc", "wc", NULL);}
-close(fd[0]);
-close(fd[1]);
-wait(0);
-wait(0);
+int fd[2];             
+pipe(fd);               
+if(!fork()) {              
+close(1);           
+close(fd[0]);       
+fcntl(fd[1], F_DUPFD, 1);   
+execlp("ls", "ls", "-l", NULL); 
+    } 
+    29     254    2059
+else { 
+close(0);           
+close(fd[1]);       
+fcntl(fd[0], F_DUPFD, 0);  
+execlp("wc", "wc", NULL);  
+    }
 
     return 0;
 }
-/* Output:    27     236    1911
- */ 
+/*Output:    29     254    2059
+ */
